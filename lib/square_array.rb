164299a0573efe365c9afb0bc4a7adb145f69070
Jun Liang <illuminatemynight@gmail.com>
2019-06-22 19:11:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,8 @@
 def square_array(array)
-  counter = 0
-  new_array = []
-  while counter < array.length do
-    number = array[counter]
-    number_squared = number ** 2
-    new_array.push(number_squared)
+  squared = []
+  count = 0 
+  while counter < array.size do
+    squared << (array[count] ** 2)
     counter += 1
   end
   new_array
